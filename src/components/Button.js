50e396a1251271b73525b76532c52d719d984952
mohammedelmala <mala_moh@msn.com>
2020-10-28 06:13:33 +0400

    use consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,17 @@ import React from "react";
 import LanguageContext from "../context/LanguageContext";
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
+    //static contextType = LanguageContext;
     render() {
-        const text = this.context === "arabic" ? "تطبيق" : "Submit";
+        //const text = this.context === "arabic" ? "تطبيق" : "Submit";
         console.log(this.context);
         return (
-            <button className="ui button primary">{text}</button>
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => value === "arabic" ? "تطبيق" : "Submit"}
+                </LanguageContext.Consumer>
+            </button>
+
         )
     }
 }
